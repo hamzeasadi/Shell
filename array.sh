94c2372e7a1e0776d284938140fcbd5e831f26b4
hamzeasadi <hamzeasadi1@gmail.com>
2022-01-14 17:44:48 +0000

    array for bash

---
+++ array.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+myarr=(1, 2, 3, 4, 7)
+
+echo ${myarr[0]}
--- intro.sh
+++ intro.sh
@@ -6,5 +6,13 @@ numparams=$#
 
 echo "the number of params = $#"
 echo "star variables save: $*"
-echo "atsign variable save: $@"
+for s in $*
+do 
+echo $s
+done
 
+echo "atsign variable save: $@"
+for a in $@
+do 
+echo $a
+done
